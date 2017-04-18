3db4fb280c8da82654155d38ae1568ab5bea53c4
TU Tran <contact@tranthanhtu.vn>
2017-04-18 14:55:55 +0700

    add gulp

---
+++ client/gulp.config.js
@@ -0,0 +1,60 @@
+module.exports = function () {
+    var root = '';
+    var app = root + 'src/';
+    var resources = root + 'resources/';
+    var index = root + 'index.html';
+    var webConfig = root + 'Web.config';
+    var assetsPath = {
+        images: resources + "images/"
+    };
+    var tsFiles = [
+        app + '**/*.ts'
+    ];
+    var dist = 'dist/';
+    var build = {
+        path: dist,
+        app: dist + 'src/'
+    };
+    var mainPath = build.path + app + 'main.js';
+
+    var systemJs = {
+        builder: {
+            normalize: true,
+            minify: false,
+            mangle: false,
+            globalDefs: { DEBUG: false }
+        }
+    };
+    var bundleName = build.path + "bundle.js";
+    var filesToCopy = [
+        { src: 'systemjs.conf.js', base:"./" },
+        { src: 'src/resources/**/*.*', base:"./"},
+        { src: 'src/themes/**/*.*', base:"./"},
+        { src: 'node_modules/@angular/**/*.*', base:"./" },
+        { src: 'node_modules/ej-angular2/**/*.*', base:"./" },
+        { src: 'node_modules/rxjs/**/*.*', base:"./" },
+        { src: 'node_modules/syncfusion-ej-global/**/*.*', base:"./" },
+        { src: 'node_modules/syncfusion-javascript/**/*.*', base:"./" },
+    ];
+    var zip = {
+        path: "dist/**/*.*",
+        archive: "deploy.zip",
+        dest: "dist"
+    };
+    var config = {
+        zip: zip,
+        files: filesToCopy,
+        bundleName: bundleName,
+        mainPath: mainPath,
+        root: root,
+        assetsPath: assetsPath,
+        app: app,
+        index: index,
+        build: build,
+        webConfig: webConfig,
+        tsFiles: tsFiles,
+        systemJs: systemJs
+    };
+
+    return config;
+};
---
+++ client/systemjs.conf.js
@@ -0,0 +1,46 @@
+System.config({
+    defaultJSExtensions: true,
+    baseUrl: '.',
+    paths: {
+        npm: "node_modules/",
+        "@app/common": ["src/modules/common/index.js"],
+        "@app/security": ["src/modules/security/index.js"],
+        "@app/setting": ["src/modules/setting/index.js"],
+        "@app/themes/default": ["src/themes/default/index.js"],
+        "@app/customerManagement": ["src/modules/customerManagement/index.js"]
+    },
+    map: {
+        "@app/common": "src/modules/common/index.js",
+        "@angular/core": 'npm@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http': 'npm@angular/http/bundles/http.umd.js',
+        '@angular/router': 'npm@angular/router/bundles/router.umd.js',
+        '@angular/forms': 'npm@angular/forms/bundles/forms.umd.js',
+        'rxjs': 'npmrxjs',
+        'ts': 'npm@plugin-typescript@4.0.10/lib/plugin.js',
+        'typescript': 'npm@typescript@2.0.3/lib/typescript.js',
+        'ej-angular2': 'npmej-angular2'
+
+    },
+    packages: {
+        "@app/common": {
+            defaultExtension: "js"
+        },
+        api: {
+            defaultExtension: "json"
+        },
+        src: {
+            main: "./main.ts",
+            defaultExtension: "js"
+        },
+        rxjs: {
+            defaultExtension: "js"
+        },
+        'ej-angular2': {
+            main: './src/index.js'
+        }
+    }
+});
